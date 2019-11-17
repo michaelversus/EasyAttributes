c2e40a70e3b2c428ebc9a0bc1fca73b57160375f
Michalis Karagiorgos <michaliskarag@gmail.com>
2019-11-17 16:00:22 +0200

    Initial commit

---
+++ Example/Pods/Target Support Files/EasyAttributes/EasyAttributes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EasyAttributes : NSObject
+@end
+@implementation PodsDummy_EasyAttributes
+@end
---
+++ Example/Pods/Target Support Files/Pods-EasyAttributes_Tests/Pods-EasyAttributes_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EasyAttributes_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_EasyAttributes_Tests
+@end
